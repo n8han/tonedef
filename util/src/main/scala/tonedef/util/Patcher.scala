45be2437481196f33e3f055779f71cd458d45583
e.e d3si9n <eed3si9n@gmail.com>
2011-05-21 18:18:01 -0400

    parse

--- util/src/main/scala/tonedef/util/Patcher.scala
+++ util/src/main/scala/tonedef/util/Patcher.scala
@@ -3,8 +3,12 @@ package tonedef.util
 import net.liftweb.json.JsonAST._
 
 class Patcher {
-  def patch(original: JObject, diff: JObject): JValue = {
-    import net.liftweb.json.JsonDSL._
+  def patchString(original: String, diff: String): String = {
+    import net.liftweb.json.{parse, compact}
+    compact(render(patch(parse(original), parse(diff))))
+  }
+
+  def patch(original: JValue, diff: JValue): JValue = {
     val merged = original merge diff
     merged transform {
       case JField(x, JNull) => JField(x, JNothing)
