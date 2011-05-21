2753f17ac892fff1c76a08c8dd6b4649870666b0
Nathan Hamblen <nathan@technically.us>
2011-05-21 16:16:25 -0400

    handle multiple listening channels

--- server/src/main/scala/Server.scala
+++ server/src/main/scala/Server.scala
@@ -5,7 +5,7 @@ import unfiltered.response._
 
 import org.jboss.netty.buffer.ChannelBuffers
 import org.jboss.netty.channel.{ChannelFuture, ChannelFutureListener}
-import org.jboss.netty.channel.group.{DefaultChannelGroup, ChannelGroupFuture}
+import org.jboss.netty.channel.group.{DefaultChannelGroup, ChannelGroupFuture, ChannelGroup}
 import org.jboss.netty.handler.codec.http.{HttpHeaders,DefaultHttpChunk}
 import org.clapper.avsl.Logger
 
@@ -14,29 +14,40 @@ class App extends unfiltered.netty.channel.Plan {
   import QParams._
 
   val logger = Logger(classOf[App])
-  private val clients = new DefaultChannelGroup
+  @volatile private var channels = Map.empty[String, ChannelGroup]
 
-  def intent = {
-    case req @ Path(Seg("channel" :: channel :: Nil)) => req match {
-      case GET(_) => 
-        logger.debug("GET channel %s" format channel)
-        val initial = req.underlying.defaultResponse(ChunkedJson)
-        val ch = req.underlying.event.getChannel
-        ch.write(initial).addListener { () =>
-          clients.add(ch)
-        }
-
-      case POST(_) =>
-        logger.debug("POST channel %s" format channel)
-        clients.write(new DefaultHttpChunk(
-          ChannelBuffers.copiedBuffer(
-            req.underlying.request.getContent,
-            ChannelBuffers.copiedBuffer("\n".getBytes("utf-8"))
-          )
-        ))
-        req.underlying.respond(Ok)
-      case _ => Pass
+  def channel(name: String) = channels.getOrElse(name, {
+    val cg = new DefaultChannelGroup
+    this.synchronized {
+      channels.getOrElse(name, {
+        channels = channels + (name -> cg)
+        cg
+      })
     }
+  })
+  def intent = {
+    case req @ Path(Seg("channel" :: name :: Nil)) => 
+      val listeners = channel(name)
+      req match {
+        case GET(_) => 
+          logger.debug("GET channel %s" format name)
+          val initial = req.underlying.defaultResponse(ChunkedJson)
+          val ch = req.underlying.event.getChannel
+          ch.write(initial).addListener { () =>
+            listeners.add(ch)
+          }
+
+        case POST(_) =>
+          logger.debug("POST channel %s" format name)
+          listeners.write(new DefaultHttpChunk(
+            ChannelBuffers.copiedBuffer(
+              req.underlying.request.getContent,
+              ChannelBuffers.copiedBuffer("\n".getBytes("utf-8"))
+            )
+          ))
+          req.underlying.respond(Ok)
+        case _ => Pass
+      }
   }
   val ChunkedJson =
     unfiltered.response.Connection(HttpHeaders.Values.CLOSE) ~>
