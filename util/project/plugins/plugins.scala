b62425fcfa3942518122c320e03a1140e1172f54
e.e d3si9n <eed3si9n@gmail.com>
2011-05-21 21:17:55 -0400

    proguard

--- util/project/plugins/plugins.scala
+++ util/project/plugins/plugins.scala
@@ -1,4 +1,5 @@
 class Plugins(info: sbt.ProjectInfo) extends sbt.PluginDefinition(info) {
   val codaRepo = "Coda Hale's Repository" at "http://repo.codahale.com/"
   val assemblySBT = "com.codahale" % "assembly-sbt" % "0.1"
+  val proguard = "org.scala-tools.sbt" % "sbt-proguard-plugin" % "0.0.5"
 }
