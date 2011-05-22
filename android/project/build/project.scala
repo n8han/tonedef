2e9a2f201c12d4875f5d154276384b35540003e2
Nathan Hamblen <nathan@technically.us>
2011-05-21 22:15:31 -0400

    util lib

--- android/project/build/project.scala
+++ android/project/build/project.scala
@@ -3,4 +3,5 @@ import sbt._
 class Project(info: ProjectInfo)
 extends AndroidProject(info: ProjectInfo) {    
   def androidPlatformName="android-8"
+  val util = "com.eed3si9n" %% "tonedef" % "1.0-SNAPSHOT"
 }
