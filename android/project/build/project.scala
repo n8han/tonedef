63f5a33a27077aacd82f782736715aba44f49c38
Nathan Hamblen <nathan@technically.us>
2011-05-21 23:59:56 -0400

    gson

--- android/project/build/project.scala
+++ android/project/build/project.scala
@@ -4,4 +4,5 @@ class Project(info: ProjectInfo)
 extends AndroidProject(info: ProjectInfo) {    
   def androidPlatformName="android-8"
   val util = "com.eed3si9n" %% "tonedef" % "1.0-SNAPSHOT"
+  val gson = "com.google.code.gson" % "gson" % "1.6"
 }
